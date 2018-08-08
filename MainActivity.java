ce6a1fed2619367b9caf2a9a955b7c57ea59a06e
ashwitha <ah00353362@techmahindra.com>
2018-08-08 14:44:29 +0530

    new add

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.creating_action_button;
+ 
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+ 
+public class MainActivity extends Activity {
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+ 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startActivity(new Intent("com.example.creating_action_button.second"));
+    //            finish();
+            }
+        });
+    }
+ 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+ 
+}
---
+++ second_activity.java
@@ -0,0 +1,14 @@
+package com.example.creating_action_button;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+ 
+public class second_activity extends Activity {
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
